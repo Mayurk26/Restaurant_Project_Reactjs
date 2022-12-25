35d16bb3ddf492128f03ff5e2c1f6bb618b9cec4
Mayur <mayurkarpe0@gmail.com>
2022-12-25 17:38:12 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+// //in this below func  all the tags must be inside the one parent tag, div or section or article or React Fragment
+// // if <React.Fragment></React.Fragment> is used then the import statement is must  short format for this is <></>
+// const App = () => {
+//   return (
+//     <div>
+//      {/* <h1 className='myclassname'>Hello world {3+3} </h1>  */}
+//       <MyName/>   {/*nested component*/}
+//       <h1 className='myclassname'>Hello world {3+3} </h1>
+//       <p>Awesome</p> 
+        
+//     </div>
+//     //<p>Awesome></p> error  -> if written adjacent to parent tag
+    
+//   )
+// }
+
+// // function App() {
+// //   return <h1>Hello World</h1>
+// // }
+
+// //react is actually looking like this
+// // const App = () =>{
+// //   return React.createElement("h1",{}, "Hello world");
+// // };
+
+// const MyName =()=>{
+//   return <h1>Mayur karpe</h1>
+// }
+
+
+// export default App
+
+
+
+
+
+//restaurant
+import React from 'react';
+import Restaurant from "./component/Basics/Restaurant";
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Restaurant/>
+  // return <UseReducer />;
+  // return <UseEffect/>;
+  // return <UseState/>;
+};
 
 export default App;
